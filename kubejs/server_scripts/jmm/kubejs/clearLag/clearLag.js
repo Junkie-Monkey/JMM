cc5253e6fcd9c536ddaa83400090325209adf57e
KuryKat <57152799+KuryKat@users.noreply.github.com>
2023-01-06 18:46:15 -0500

    clearLag script

---
+++ kubejs/server_scripts/jmm/kubejs/clearLag/clearLag.js
@@ -0,0 +1,88 @@
+//priority: 1100
+
+let runCL = true
+const minutes = 30
+const allowlist = Ingredient.matchAny([
+    `minecraft:diamond`,
+    `minecraft:gold_ingot`,
+    `minecraft:netherite_scrap`,
+    `minecraft:netherite_ingot`,
+    `minecraft:emerald`,
+    `#forge:ores`,
+    `@tinkersconstruct`,
+    `@draconicevolution`,
+    `@mekanism`
+])
+
+const lastClearResult = Utils.newCountingMap()
+
+/**
+ * Clears the items that are dropped on the ground (that are not in the allowlist)
+ * @param {ServerJS} server 
+ */
+const clearLag = (server) => {
+    const itemList = server.getEntities(`@e[type=item]`)
+    lastClearResult.clear()
+
+    itemList.forEach(entity => {
+        if (!allowlist.test(entity.item)) {
+            const itemName = entity.item.name
+            const itemCount = entity.item.count
+            lastClearResult.add(itemName, itemCount)
+            entity.kill()
+        }
+    })
+
+    server.tell([
+        Text.lightPurple('[ClearLag]'),
+        ` Removed ${lastClearResult.totalCount / 3} items from the ground.`
+    ])
+}
+
+/**
+ * Schedules the clearLag function to be executed every X minutes [Default = every 30 minutes]
+ * @param {ServerJS} server 
+ */
+const clearLagLoop = (server) => {
+    server.tell([Text.lightPurple('[ClearLag]'), ` Timer started!`])
+    server.tell([Text.lightPurple('[ClearLag]'), ` Removing all items on ground in ${minutes} minutes!`])
+    server.schedule(MINUTE * (minutes - 6), (callback) => {
+        if (runCL) {
+            server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 5 minutes!'])
+            server.schedule(MINUTE * 4, () => {
+                if (runCL) {
+                    server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 1 minute!'])
+                    server.schedule(SECOND * 50, () => {
+                        if (runCL) {
+                            server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 10 seconds!'])
+                            server.schedule(SECOND * 7, () => {
+                                if (runCL) {
+                                    server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 3 seconds!'])
+                                    server.schedule(SECOND * 1, () => {
+                                        if (runCL) {
+                                            server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 2 seconds!'])
+                                            server.schedule(SECOND * 1, () => {
+                                                if (runCL) {
+                                                    server.tell([Text.lightPurple('[ClearLag]'), ' Removing all items on ground in 1 second!'])
+                                                    server.schedule(SECOND * 1, () => clearLag(server))
+                                                }
+                                            })
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+            callback.reschedule()
+        }
+    })
+}
+
+onEvent('server.load', event => {
+    if (!runCL) return
+    event.server.schedule(SECOND * 5, () => {
+        clearLagLoop(event.server)
+    })
+})
