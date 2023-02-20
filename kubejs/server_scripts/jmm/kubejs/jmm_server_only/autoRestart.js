b563102c940688cb5e435689c1537200f7659b6e
Amaro "KuryKat" Alexandre <57152799+KuryKat@users.noreply.github.com>
2023-02-20 14:27:22 -0300

    Update autoRestart.js

--- kubejs/server_scripts/jmm/kubejs/jmm_server_only/autoRestart.js
+++ kubejs/server_scripts/jmm/kubejs/jmm_server_only/autoRestart.js
@@ -1,45 +1,22 @@
 //priority: 1100
-
-let runAR = true;
-let minutesAR = 1440; // One Day
+let runAR = true
+let oneDay = MINUTE * 60 * 24
 
 const restartLoop = (server) => {
-    server.tell([Text.lightPurple('[AutoRestart]'), ` Timer started!`]);
-    server.tell([Text.lightPurple('[AutoRestart]'), ` Server will automatically restart in ${minutesAR} minutes!`]);
+    server.tell([Text.lightPurple('[AutoRestart]'), ` Timer started!`])
+    server.tell([Text.lightPurple('[AutoRestart]'), ` Server will automatically restart in ${Math.floor(oneDay / 1200)} minutes!`])
 
-    server.schedule(MINUTE * (minutesAR - 6), () => {
-        server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 5 minutes!']);
-        server.schedule(MINUTE * 4, () => {
-            server.runCommand(`tellraw @a "§d[AutoRestart]§r Server will automatically restart in 1 minute!"`);
-            server.schedule(SECOND * 50, () => {
-                server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 10 seconds!']);
-                server.schedule(SECOND * 7, () => {
-                    server.tell([
-                        Text.lightPurple('[AutoRestart]'),
-                        ' Server will automatically restart in 3 seconds!'
-                    ]);
-                    server.schedule(SECOND * 1, () => {
-                        server.tell([
-                            Text.lightPurple('[AutoRestart]'),
-                            ' Server will automatically restart in 2 seconds!'
-                        ]);
-                        server.schedule(SECOND * 1, () => {
-                            server.tell([
-                                Text.lightPurple('[AutoRestart]'),
-                                ' Server will automatically restart in 1 second!'
-                            ]);
-                            server.schedule(SECOND * 1, () => server.runCommand('stop'));
-                        });
-                    });
-                });
-            });
-        });
-    });
-};
+    server.schedule(oneDay - (MINUTE * 5), () => server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 5 minutes!']))
+    server.schedule(oneDay - (MINUTE), () => server.runCommand(`tellraw @a "§d[AutoRestart]§r Server will automatically restart in 1 minute!"`))
+    server.schedule(oneDay - (SECOND * 10), () => server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 10 seconds!']))
+    server.schedule(oneDay - (SECOND * 3), () => server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 3 seconds!']))
+    server.schedule(oneDay - (SECOND * 2), () => server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 2 seconds!']))
+    server.schedule(oneDay - (SECOND), () => server.tell([Text.lightPurple('[AutoRestart]'), ' Server will automatically restart in 1 second!']))
+    server.schedule(oneDay, () => server.runCommand('stop'))
+}
 
-onEvent('server.load', function (event) {
-    if (!runAR) return;
-    event.server.schedule(SECOND * 5, () => {
-        restartLoop(event.server);
-    });
-});
+onEvent('server.load', event => {
+    if (runAR) {
+        restartLoop(event.server)
+    }
+})
